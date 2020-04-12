cd69653ae126b64e710b08054eb030d8205524a0
Daniel Visca <danielvisca96@gmail.com>
2020-04-11 20:52:58 -0400

    dotnet setup

---
+++ firstDotNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace firstDotNet
+{
+    class Program
+    {
+        static void Test() 
+        {
+            decimal a = 2.0M;
+            for (int i = 0; i < 10; i++){
+                Console.WriteLine(a);
+                a *= a;
+            }
+            Console.WriteLine(a);
+        }
+        static void Main(string[] args)
+        {
+            Test();
+        }
+    }
+}
